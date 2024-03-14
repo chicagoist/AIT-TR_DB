42953793403dab4f15f87380a4822e8813443e87
chicagoist <z7t35gbba@mozmail.com>
2024-03-14 13:12:04 +0100

    query to MongoDB

--- src/Lesson_04/classwork/users.mongodb.js
+++ src/Lesson_04/classwork/users.mongodb.js
@@ -1,2 +1,15 @@
 use("school_db");
-db.users.insertOne({ name: "Valerii"});
+
+
+// db.users.insertOne({ name: "Valerii"});
+
+// db.users.insertOne({ 
+//     name: "Valerii",
+//     address: {
+//         city: "Frankfurt am Main",
+//         country: "Deutschland",
+//     },
+
+// });
+
+db.users.find();
